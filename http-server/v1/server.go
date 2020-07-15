e5f80b02fbebd95c876f10ebf9e88d62947ed052
Adry Joseph-Augustin <35359788+adryja972@users.noreply.github.com>
2020-07-15 18:13:01 -0400

    Update server.go

--- http-server/v1/server.go
+++ http-server/v1/server.go
@@ -7,5 +7,5 @@ import (
 
 // PlayerServer currently returns Hello, world given _any_ request.
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	fmt.Fprint(w, "Hello DevOps")
 }
